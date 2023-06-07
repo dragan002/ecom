761dd1f58460d21b5c9c21b79a8834eb2ff05e1b
dragan002 <draganvujic29@gmail.com>
2023-06-07 14:02:35 +0200

    *creating picture part(slider, buttons etc)

--- index.php
+++ index.php
@@ -6,11 +6,110 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styless.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>::Ecommerce</title>
 </head>
 
 <body>
+    <div class="wrapper">
+        <div class="navigation-bar">
+            <div class="container">
+                <div class="logo">
+                    <img src="images/logo.png" alt="currently not available">
+                </div>
 
+                <!--search box -->
+                <div class="search_area">
+                    <form action="" method="post">
+                        <input type="text" name="search_box" class="search_box" placeholder="Search all items">
+                        <button class="search_btn btn btn-success"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+
+                <!-- User menu -->
+                <div class="user_menu">
+                    <ul>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-hoshpopup="true" aria-expanded="false"> <i class="fas fa-user"></i> My Account</a>
+                            <!-- Dropdown box -->
+                            <div class="dropdown-menu dropdown-menu-right bg-dark" style="width:250px">
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-user"></i> &nbsp; Your Account</button></a>
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-cube"></i> &nbsp; Your orders</button></a>
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-heart"></i> &nbsp; Wishlist</button></a>
+                                <div class="dropdown-divider"></div>
+                                <p class="text-center text-white" style="height:15px;line-height:20px;"><small>If you are new user</small></p>
+
+                                <a href="#"><button type="button" class="dropdown-item text-center"><i class="fas fa-user"></i> &nbsp; Register</button></a>
+                                <a href="#"><button type="button" class="dropdown-item text-center bg-danger"><i class="fas fa-user"></i> &nbsp; Login</button></a>
+                            </div>
+
+
+
+                        </li>
+
+
+                        <li><a href="#">Cart <i class="fas fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- slider-area -->
+        <div class="slider-area">
+            <div class="slider">
+                <div>
+                    <a href="">
+                        <img src="images/pexels-mart-production-7679682.jpg" alt="">
+                    </a>
+                    <!-- Slider content -->
+                    <div class="slider-content">
+                        <h3 class="text-white text-capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, dolorem?</h3>
+                        <a href="#"><button class="btn btn-primary"> <i class="fas fa-shopping-cart"></i> Buy Now</button></a>
+                        <a href="#"><button class="btn btn-outline-danger ml-5">Read More</button></a>
+                    </div>
+                </div>
+
+                <div>
+                    <a href="">
+                        <img src="images/pexels-sam-lion-5709661.jpg" alt="">
+                    </a>
+
+                    <div class="slider-content">
+                        <h3 class="text-white text-capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, dolorem?</h3>
+                        <a href="#"><button class="btn btn-primary"> <i class="fas fa-shopping-cart"></i> Buy Now</button></a>
+                        <a href="#"><button class="btn btn-outline-danger ml-5">Read More</button></a>
+                    </div>
+                </div>
+
+                <div>
+                    <a href="">
+                        <img src="images/pexels-timothy-paule-ii-2002717 (1).jpg" alt="">
+                    </a>
+
+                    <div class="slider-content">
+                        <h3 class="text-white text-capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, dolorem?</h3>
+                        <a href="#"><button class="btn btn-primary"> <i class="fas fa-shopping-cart"></i> Buy Now</button></a>
+                        <a href="#"><button class="btn btn-outline-danger ml-5">Read More</button></a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <i class="fas fa-camera-retro"></i>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
