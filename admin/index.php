f94910121f91c6eb9b560677c9eb561f36015635
dragan002 <draganvujic29@gmail.com>
2023-06-08 14:28:46 +0200

    **Dashboard design

--- admin/index.php
+++ admin/index.php
@@ -36,6 +36,144 @@
         </div>
     </header>
 
+    <div class="wrapper">
+        <!-- sidebar -->
+        <section class="sidebar">
+            <ul class="nav-bar">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li><a href="#"><i class="fa fa-dolly-flatbed"></i> Brands</a></li>
+                <li><a href="#"><i class="fa fa-dolly"></i> Category</a></li>
+                <li><a href="#"><i class="fa fa-shopping-basket"></i> Products</a></li>
+                <li><a href="#"><i class="fa fa-truck"></i> Orders</a></li>
+                <li><a href="#"><i class="fa fa-truck-loading"></i> Delivery</a></li>
+                <li><a href="#"><i class="fa fa-images"></i> Slider image</a></li>
+                <li><a href="#"><i class="fa fa-cogs"></i> Settings</a></li>
+                <li><a href="#"><i class="fa fa-id-badge"></i> Profile</a></li>
+                <li><a href="#"><i class="fa fa-sign-out-alt"></i> Logout</a></li>
+            </ul>
+        </section>
+        <!-- Workingpanel -->
+        <section class="working-panel">
+            <div class="container-fluid">
+                <h1 class="display-4">Welcome to the Dashboard</h1>
+                <hr>
+                <div class="row">
+                    <!-- category widget -->
+                    <div class="col-md-3">
+                        <div class="card bg-orange-g text-white">
+                            <div class="card-body">
+                                <h3 class="font-weight-light"> <i class="fa fa-dolly"></i> Category</h3>
+                                <hr>
+                                <h5>
+                                    <b>123456</b>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- brands -->
+                    <div class="col-md-3">
+                        <div class="card bg-green-g text-white">
+                            <div class="card-body">
+                                <h3 class="font-weight-light"> <i class="fa fa-dolly-flatbed"></i>All brands</h3>
+                                <hr>
+                                <h5>
+                                    <b>123456</b>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- All users -->
+                    <div class="col-md-3">
+                        <div class="card bg-primary-g text-white">
+                            <div class="card-body">
+                                <h3 class="font-weight-light"> <i class="fa fa-users"></i>All Users</h3>
+                                <hr>
+                                <h5>
+                                    <b>123456</b>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- All Authors -->
+
+                    <div class="col-md-3">
+                        <div class="card bg-golden-g text-white">
+                            <div class="card-body">
+                                <h3 class="font-weight-light"> <i class="fa fa-truck"></i>All Orders</h3>
+                                <hr>
+                                <h5>
+                                    <b>300</b>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <!-- All order table -->
+                <div class="all-order mt-5">
+                    <hr>
+                    <h2>New Orders</h2>
+                    <hr>
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr class="bg-primary text-white">
+                                <th scope="col">Order No.</th>
+                                <th scope="col">Order name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Paid Status</th>
+                                <th scope="col">Order Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>3</td>
+                                <td>23/09/23</td>
+                                <td><span class="badge badge-danger">UnPaid</span></td>
+                                <td><span class="badge badge-success">Complete</span></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>2</td>
+                                <td>22/10/2022</td>
+                                <td><span class="badge badge-success">Paid</span></td>
+                                <td><span class="badge badge-info">Process</span></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>5</td>
+                                <td>02/05/2022</td>
+                                <td><span class="badge badge-success">Paid</span></td>
+                                <td><span class="badge badge-danger">Rejected</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <!-- pagination -->
+                    <div class="order-pagination">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
     <script src="../js/jquery.js"></script>
 
     <script src="../js/bootstrap.bundle.min.js"></script>
